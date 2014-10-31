d2da393a1a13845800fe6c21b842bdc410b630d6
Vivian Huang <lingihuang@hotmail.com>
2014-10-31 18:54:31 +0800

    Update php code of upload

--- uploader/upload.php
+++ uploader/upload.php
@@ -19,6 +19,15 @@ function getFiles($fileArr)
 function moveFiles($files)
 {
 	$isOk = true;
+
+	// if (!file_exists('files/'))
+	// {
+	// 	if (!mkdir('files', 0777, true))
+	// 	{
+	// 		die('Failed to create directories...');
+	// 	}
+	// }
+
 	foreach ($files as $file)
 	{
 		if (move_uploaded_file($file['tmp_name'], 'files/'. $file['name']))
@@ -33,12 +42,39 @@ function moveFiles($files)
 	return $isOk;
 }
 
-$files  = getFiles($_FILES['files']);
-$status = moveFiles($files) ? 'ok' : 'failed';
-
 // Check if this is an AJAX request.
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']))
 {
+	if (isset($_FILES['files']))
+	{
+		// Upload all files.
+		$files  = getFiles($_FILES['files']);
+		$status = moveFiles($files) ? 'ok' : 'failed';
+	}
+	else
+	{
+		// Upload single file.
+		$fileName = $_SERVER['HTTP_X_FILE_NAME'];
+		$fileSize = $_SERVER['HTTP_X_FILE_SIZE'];
+		$fileType = $_SERVER['HTTP_X_FILE_TYPE'];
+		$input    = fopen('php://input', 'r');
+		$output   = fopen('files/' . $fileName, 'a');
+ 
+ 		while ($data = fread($input, 1024))
+ 		{
+ 			fwrite($output, $data);
+ 		}
+ 		fclose($input);
+ 		fclose($output);
+
+ 		if (filesize('files/' . $fileName) !== $fileSize)
+ 		{
+ 			unlink('files/' . $fileName);
+ 		}
+
+ 		$status = 'ok';
+	}
+
 	$response = array(
 		'status' => $status
 	);
@@ -48,6 +84,7 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']))
 }
 else
 {
+	// Upload with iFrame for old browsers.
 	echo $status;
 }
 
